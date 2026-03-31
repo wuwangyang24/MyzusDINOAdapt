c64c26a032d6f29eb9213e229576d1fe5565f6dc
Wu Wang Yang <wuwangyang24@gmail.com>
2026-03-31 10:39:21 +0200

    Include metadata file count in checkpoint folder name

--- scripts/train.py
+++ scripts/train.py
@@ -576,6 +576,8 @@ def main():
     lr = config["training"]["learning_rate"]
     lr_str = f"{lr:.0e}" if lr < 0.001 else f"{lr:g}"
     ckpt_tag = f"{backbone}_{adaptation_method}_r{r}a{alpha_str}-T{args.repulsion_temperature}-LR{lr_str}"
+    if len(args.metadata) > 1:
+        ckpt_tag += f"_meta{len(args.metadata)}"
     if args.control_embeddings:
         ckpt_tag += "_PreCon"
     if config[adaptation_method].get("train_layernorm", False):
